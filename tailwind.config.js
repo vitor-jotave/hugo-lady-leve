0c1709d6e8278844e156df7681e83df6c0dccf20
João Vitor <joaovitors@live.com>
2024-11-24 22:25:13 -0300

    deploy for pagespeed test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,22 @@
-const typography = require('@tailwindcss/typography');
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./hugo_stats.json'],
-	plugins: [typography],
+	content: ["./layouts/**/*.html", "./content/**/*.{html,md}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Montserrat'],
+				system: ['system-ui']
+			},
+			colors: {
+				green: {
+					500: '#4db54c'
+				},
+				orange: {
+					300: '#FFA100',
+					500: '#ff7f00'
+				}
+			},
+		},
+	},
+	plugins: [],
 };
